654800d1cc95b7c96d04730dfd4cc52c54efbff3
microchips-n-dip <xisavariable52@gmail.com>
2019-06-11 23:38:34 -0400

    Nearly complete state machine table for new lexer.

--- lex2.c
+++ lex2.c
@@ -1,9 +1,49 @@
+enum equivalence_classes {
+  EQCLASS_WHITESPACE,
+  EQCLASS_NEWLINE,
+  EQCLASS_LETTER,
+  EQCLASS_NUMBER,
+  EQCLASS_EQUALS,
+  EQCLASS_COLON,
+  EQCLASS_MINUS,
+  EQCLASS_DOT,
+  EQCLASS_GT,
+  EQCLASS_OPERATOR
+};
+
+static const char
+equivalence_class[256] = {
+  [' ']         = EQCLASS_WHITESPACE,
+  ['\t']        = EQCLASS_WHITESPACE,
+  ['\n']        = EQCLASS_NEWLINE,
+  ['A' ... 'Z'] = EQCLASS_LETTER,
+  ['a' ... 'z'] = EQCLASS_LETTER,
+  ['0' ... '9'] = EQCLASS_NUMBER,
+  ['=']         = EQCLASS_EQUALS,
+  [':']         = EQCLASS_COLON,
+  ['+']         = EQCLASS_OPERATOR,
+  ['-']         = EQCLASS_MINUS,
+  ['*']         = EQCLASS_OPERATOR,
+  ['/']         = EQCLASS_OPERATOR,
+  ['.']         = EQCLASS_DOT,
+  ['>']         = EQCLASS_GT,
+  ['<']         = EQCLASS_OPERATOR,
+};
+
 enum lexer_state {
   /* Terminal states. */
-  LS_STD_TERMINAL,
-  LS_TERMINALS,    /* End of terminal state enumeration. */
+  LS_NEWLINE_TERMINAL  = -10,
+  LS_IDENT_TERMINAL    = -9,
+  LS_NUMBER_TERMINAL   = -8,
+  LS_EQUALS_TERMINAL   = -7,
+  LS_COLON_TERMINAL    = -6,
+  LS_RARROW_TERMINAL   = -5,
+  LS_IMPURE_TERMINAL   = -4,
+  LS_ELLIPSIS_TERMINAL = -3,
+  LS_OPERATOR_TERMINAL = -2,
+  LS_TERMINALS         = -1,
+  LS_START             =  0,
   LS_WHITESPACE,   /* Whitespace, to be skipped. */
-  LS_START,
   /* Normal textual lexing. */
   LS_IDENT,
   LS_NUMBER,
@@ -11,149 +51,176 @@ enum lexer_state {
   LS_CHAR,
   /* Operator lexing states. */
   /* Single char operators. */
-  LS_SEEN_EQUALS, /* "=" */
-  LS_SEEN_GT,     /* ">" */
-  LS_SEEN_LT,     /* "<" */
-  LS_SEEN_DOT,    /* "." */
-  LS_SEEN_COLON,  /* ":" */
-  LS_SEEN_PLUS,   /* "+" */
-  LS_SEEN_MINUS,  /* "-" */
-  LS_SEEN_MUL,    /* "*" */
-  LS_SEEN_DIV,    /* "/" */
-  /* Double char operators. */
-  LS_SEEN_LSHIFT, /* "<<" */
-  LS_SEEN_RSHIFT, /* ">>" */
-  LS_SEEN_INCR,   /* "++" */
-  LS_SEEN_DECR,   /* "--" */
-  LS_SEEN_ACCADD, /* "+=" */
-  LS_SEEN_ACCSUB, /* "-=" */
-  LS_SEEN_ACCMUL, /* "*=" */
-  LS_SEEN_ACCDIV, /* "/=" */
-  LS_SEEN_EQEQ,   /* "==" */
-  LS_SEEN_GTEQ,   /* ">=" */
-  LS_SEEN_LTEQ,   /* "<=" */
-  LS_SEEN_RARROW, /* "->" */
-  LS_SEEN_IMPURE, /* "=>" */
-  LS_SEEN_ELLIPSIS_1, /* ".." */
+  LS_SEEN_EQUALS,   /* "=" */
+  LS_SEEN_COLON,    /* ":" */
+  LS_SEEN_MINUS,    /* "-" */
+  LS_SEEN_RARROW,   /* "->" */
+  LS_SEEN_IMPURE,   /* "=>" */
+  LS_SEEN_DOT,      /* "." */
+  LS_SEEN_DOT_2,    /* ".." */
   /* Triple char operators. */
   LS_SEEN_ELLIPSIS, /* "..." */
-  LS_OPERATOR,    /* Generic operator. */
-  LS_SEEN_TICK,   /* "'" */
-  /* Last lexer state. */
-  LS_LENGTH
+  LS_OPERATOR,      /* Generic operator. */
+  LS_SEEN_TICK      /* "'" */
 };
 
-struct lexer_transition {
-  enum lexer_state state : 8;
-  enum token_type type   : 8;
-};
+/* Total number of states. */
 
-static const struct lexer_transition
+static const char LS_LENGTH = LS_SEEN_TICK;
+
+/* State machine transitions table. */
+
+static const signed char
 transitions[LS_LENGTH][EQCLASS_LENGTH] = {
   /* Initial transitions. */
-  /* Start of ident. */
-  [LS_START][EQCLASS_IDENT]         = {LS_IDENT, TOKEN_NAME},
-  /* Start of number. */
-  [LS_START][EQCLASS_NUMBER]        = {LS_NUMBER, TOKEN_NUMBER},
-  /* Elementary operator encounters. */
-  [LS_START][EQCLASS_PLUS]          = {LS_SEEN_PLUS, TOKEN_PLUS},
-  [LS_START][EQCLASS_MINUS]         = {LS_SEEN_MINUS, TOKEN_MINUS},
-  [LS_START][EQCLASS_MUL]           = {LS_SEEN_MUL, TOKEN_MUL},
-  [LS_START][EQCLASS_DIV]           = {LS_SEEN_DIV, TOKEN_DIV},
-  /* Encountered '='. */
-  [LS_START][EQCLASS_EQUALS]        = {LS_SEEN_EQUALS, TOKEN_EQUALS},
-  /* Elementary comparison encounters. */
-  [LS_START][EQCLASS_GT]            = {LS_SEEN_GT, TOKEN_GT},
-  [LS_START][EQCLASS_LT]            = {LS_SEEN_LT, TOKEN_LT},
-  /* Misc. */
-  [LS_START][EQCLASS_DOT]           = {LS_SEEN_DOT, TOKEN_DOT},
-  [LS_START][EQCLASS_COLON]         = {LS_SEEN_COLON, TOKEN_COLON},
-  [LS_START][EQCLASS_TICK]          = {LS_SEEN_TICK, TOKEN_TICK},
+  [LS_START][EQCLASS_WHITESPACE]       = LS_WHITESPACE,
+  [LS_START][EQCLASS_NEWLINE]          = LS_NEWLINE,
+  [LS_START][EQCLASS_IDENT]            = LS_IDENT,
+  [LS_START][EQCLASS_NUMBER]           = LS_NUMBER,
+  [LS_START][EQCLASS_EQUALS]           = LS_SEEN_EQUALS,
+  [LS_START][EQCLASS_COLON]            = LS_SEEN_COLON,
+  [LS_START][EQCLASS_MINUS]            = LS_SEEN_MINUS,
+  [LS_START][EQCLASS_DOT]              = LS_SEEN_DOT,
+  [LS_START][EQCLASS_GT]               = LS_OPERATOR,
+  [LS_START][EQCLASS_OPERATOR]         = LS_OPERATOR,
+  [LS_START][EQCLASS_TICK]             = LS_SEEN_TICK,
+  /* Whitespace transitions. */
+  [LS_WHITESPACE][EQCLASS_WHITESPACE]  = LS_WHITESPACE,
+  [LS_WHITESPACE][EQCLASS_NEWLINE]     = LS_WHITESPACE,
+  [LS_WHITESPACE][EQCLASS_IDENT]       = LS_START,
+  [LS_WHITESPACE][EQCLASS_NUMBER]      = LS_START,
+  [LS_WHITESPACE][EQCLASS_EQUALS]      = LS_START,
+  [LS_WHITESPACE][EQCLASS_COLON]       = LS_START,
+  [LS_WHITESPACE][EQCLASS_MINUS]       = LS_START,
+  [LS_WHITESPACE][EQCLASS_DOT]         = LS_START,
+  [LS_WHITESPACE][EQCLASS_GT]          = LS_START,
+  [LS_WHITESPACE][EQCLASS_OPERATOR]    = LS_START,
+  [LS_NEWLINE][EQCLASS_WHITESPACE]     = LS_NEWLINE_TERMINAL,
+  [LS_NEWLINE][EQCLASS_NEWLINE]        = LS_NEWLINE,
+  [LS_NEWLINE][EQCLASS_IDENT]          = LS_NEWLINE_TERMINAL,
+  [LS_NEWLINE][EQCLASS_NUMBER]         = LS_NEWLINE_TERMINAL,
+  [LS_NEWLINE][EQCLASS_EQUALS]         = LS_NEWLINE_TERMINAL,
+  [LS_NEWLINE][EQCLASS_COLON]          = LS_NEWLINE_TERMINAL,
+  [LS_NEWLINE][EQCLASS_MINUS]          = LS_NEWLINE_TERMINAL,
+  [LS_NEWLINE][EQCLASS_DOT]            = LS_NEWLINE_TERMINAL,
+  [LS_NEWLINE][EQCLASS_GT]             = LS_NEWLINE_TERMINAL,
+  [LS_NEWLINE][EQCLASS_OPERATOR]       = LS_NEWLINE_TERMINAL,
   /* Normal textual transitions. */
-  [LS_IDENT][EQCLASS_IDENT]         = {LS_IDENT, TOKEN_NAME},
-  [LS_IDENT][EQCLASS_NUMBER]        = {LS_IDENT, TOKEN_NAME},
-  [LS_NUMBER][EQCLASS_NUMBER]       = {LS_NUMBER, TOKEN_NUMBER},
+  [LS_IDENT][EQCLASS_WHITESPACE]       = LS_IDENT_TERMINAL,
+  [LS_IDENT][EQCLASS_NEWLINE]          = LS_IDENT_TERMINAL,
+  [LS_IDENT][EQCLASS_IDENT]            = LS_IDENT,
+  [LS_IDENT][EQCLASS_NUMBER]           = LS_IDENT,
+  [LS_IDENT][EQCLASS_EQUALS]           = LS_IDENT_TERMINAL,
+  [LS_IDENT][EQCLASS_COLON]            = LS_IDENT_TERMINAL,
+  [LS_IDENT][EQCLASS_MINUS]            = LS_IDENT_TERMINAL,
+  [LS_IDENT][EQCLASS_DOT]              = LS_IDENT_TERMINAL,
+  [LS_IDENT][EQCLASS_GT]               = LS_IDENT_TERMINAL,
+  [LS_IDENT][EQCLASS_OPERATOR]         = LS_IDENT_TERMINAL,
+  [LS_IDENT][EQCLASS_TICK]             = LS_IDENT_TERMINAL,
+  [LS_NUMBER][EQCLASS_WHITESPACE]      = LS_NUMBER_TERMINAL,
+  [LS_NUMBER][EQCLASS_NEWLINE]         = LS_NUMBER_TERMINAL,
+  [LS_NUMBER][EQCLASS_IDENT]           = LS_NUMBER_TERMINAL,
+  [LS_NUMBER][EQCLASS_NUMBER]          = LS_NUMBER,
+  [LS_NUMBER][EQCLASS_EQUALS]          = LS_NUMBER_TERMINAL,
+  [LS_NUMBER][EQCLASS_COLON]           = LS_NUMBER_TERMINAL,
+  [LS_NUMBER][EQCLASS_MINUS]           = LS_NUMBER_TERMINAL,
+  [LS_NUMBER][EQCLASS_DOT]             = LS_NUMBER_TERMINAL,
+  [LS_NUMBER][EQCLASS_GT]              = LS_NUMBER_TERMINAL,
+  [LS_NUMBER][EQCLASS_OPERATOR]        = LS_NUMBER_TERMINAL,
+  [LS_NUMBER][EQCLASS_TICK]            = LS_NUMBER_TERMINAL,
   /* TODO: Strings and chars. */
   /* Operator transitions. */
-  /* Encountered '+' then '+'. */
-  [LS_SEEN_PLUS][EQCLASS_PLUS]      = {LS_SEEN_INCR, TOKEN_INCR},
-  /* Encountered '+' then '='. */
-  [LS_SEEN_PLUS][EQCLASS_EQUALS]    = {LS_SEEN_ACCADD, TOKEN_ACCADD},
-  /* Encountered '+' then any of {'-', '*', '/'}. */
-  [LS_SEEN_PLUS][EQCLASS_MINUS ... EQCLASS_DIV]
-                                    = {LS_OPERATOR, TOKEN_OPERATOR},
-  /* Encountered '+' then any of {'>', '<', ':'}. */
-  [LS_SEEN_PLUS][EQCLASS_GT ... EQCLASS_COLON]
-                                    = {LS_OPERATOR, TOKEN_OPERATOR},
-  /* Encountered '-' then '-'. */
-  [LS_SEEN_MINUS][EQCLASS_MINUS]    = {LS_SEEN_DECR, TOKEN_DECR},
-  /* Encountered '-' then '='. */
-  [LS_SEEN_MINUS][EQCLASS_EQUALS]   = {LS_SEEN_ACCSUB, TOKEN_ACCSUB},
-  /* Encountered '-' then any of {'>, '<', ':', '+'}. */
-  [LS_SEEN_MINUS][EQCLASS_GT ... EQCLASS_PLUS]
-                                    = {LS_OPERATOR, TOKEN_OPERATOR},
-  /* Encountered '-' then any of {'*', '/'}. */
-  [LS_SEEN_MINUS][EQCLASS_MUL ... EQCLASS_DIV]
-                                    = {LS_OPERATOR, TOKEN_OPERATOR},
-  /* Encountered '*' then '='. */
-  [LS_SEEN_MUL][EQCLASS_EQUALS]     = {LS_SEEN_ACCMUL, TOKEN_ACCMUL},
-  /* Encountered '*' then any of {'>', '<', ':', '+', '-', '*', '/'}. */
-  [LS_SEEN_MUL][EQCLASS_GT ... EQCLASS_DIV]
-                                    = {LS_OPERATOR, TOKEN_OPERATOR},
-  /* Encountered '/' then '='. */
-  [LS_SEEN_DIV][EQCLASS_EQUALS]     = {LS_SEEN_ACCDIV, TOKEN_ACCDIV},
-  /* Encountered '/' then any of {'>', '<', ':', '+', '-', '*', '/'}. */
-  [LS_SEEN_DIV][EQCLASS_GT ... EQCLASS_DIV]
-                                    = {LS_OPERATOR, TOKEN_OPERATOR},
-  /* Encountered '=' then '='. */
-  [LS_SEEN_EQUALS][EQCLASS_EQUALS]  = {LS_SEEN_EQEQ, TOKEN_EQEQ},
-  /* Encountered '=' then '>'. */
-  [LS_SEEN_EQUALS][EQCLASS_GT]      = {LS_SEEN_IMPURE, TOKEN_IMPURE},
-  /* Encountered '=' then any of {'<', ':', '+', '-', '*', '/'}. */
-  [LS_SEEN_EQUALS][EQCLASS_LT ... EQCLASS_DIV]
-                                    = {LS_OPERATOR, TOKEN_OPERATOR},
-  /* Encountered '>' then '='. */
-  [LS_SEEN_GT][EQCLASS_EQUALS]      = {LS_SEEN_GTEQ, TOKEN_GTEQ},
-  /* Encountered '>' then '>'. */
-  [LS_SEEN_GT][EQCLASS_GT]          = {LS_SEEN_RSHIFT, TOKEN_RSHIFT},
-  /* Encountered '>' then any of {'<', ':', '+', '-', '*', '/'}. */
-  [LS_SEEN_GT][EQCLASS_LT ... EQCLASS_DIV]
-                                    = {LS_OPERATOR, TOKEN_OPERATOR},
-  /* Encountered '<' then '='. */
-  [LS_SEEN_LT][EQCLASS_EQUALS]      = {LS_SEEN_LTEQ, TOKEN_LTEQ},
-  /* Encountered '<' then '<'. */
-  [LS_SEEN_LT][EQCLASS_LT]          = {LS_SEEN_LSHIFT, TOKEN_LSHIFT},
-  /* Encountered '<' then '>'. */
-  [LS_SEEN_LT][EQCLASS_GT]          = {LS_OPERATOR, TOKEN_OPERATOR},
-  /* Encountered '<' then any of {':', '+', '-', '*', '/'}. */
-  [LS_SEEN_LT][EQCLASS_COLON ... EQCLASS_DIV]
-                                    = {LS_OPERATOR, TOKEN_OPERATOR}
-  /* Encountered ':' then anything. */
-  [LS_SEEN_COLON][EQCLASS_COLON ... EQCLASS_DIV]
-                                    = {LS_OPERATOR, TOKEN_OPERATOR}
-  /* Encountered '.' then '.'. */
-  [LS_SEEN_DOT][EQCLASS_DOT]        = {LS_SEEN_ELLIPSIS_1, TOKEN_OPERATOR},
-  /* Encountered '.' then any of {'=', '>', '<'}. */
-  [LS_SEEN_DOT][EQCLASS_EQUALS ... EQCLASS_LT]
-                                    = {LS_OPERATOR, TOKEN_OPERATOR},
-  /* Encountered '.' then any of {':', '+', '-', '*', '/'}. */
-  [LS_SEEN_DOT][EQCLASS_COLON ... EQCLASS_DIV]
-                                    = {LS_OPERATOR, TOKEN_OPERATOR},
-  /* All of the double char operators can just go to LS_OPERATOR if they
-     encounter anything else. */
-  [LS_SEEN_LSHIFT ... LS_SEEN_IMPURE][EQCLASS_EQUALS ... EQCLASS_DIV]
-                                    = {LS_OPERATOR, TOKEN_OPERATOR},
-  /* Encountered ".." then '.'. */
-  [LS_SEEN_ELLIPSIS_1][EQCLASS_DOT] = {LS_SEEN_ELLIPSIS, TOKEN_ELLIPSIS},
-  /* Encountered ".." then any of {'=', '>', '<'}. */
-  [LS_SEEN_ELLIPSIS_1][EQCLASS_EQUALS ... EQCLASS_LT]
-                                    = {LS_OPERATOR, TOKEN_OPERATOR},
-  /* There are no 4-char operators so just deal with this, yeah? */
-  [LS_SEEN_ELLIPSIS][EQCLASS_EQUALS ... EQCLASS_DIV]
-                                    = {LS_OPERATOR, TOKEN_OPERATOR},
-  /* Finally, for things that are already unrecognized operators. */
-  [LS_OPERATOR][EQCLASS_EQUALS ... EQCLASS_DIV]
-                                    = {LS_OPERATOR, TOKEN_OPERATOR},
+  [LS_SEEN_EQUALS][EQCLASS_WHITESPACE] = LS_EQUALS_TERMINAL,
+  [LS_SEEN_EQUALS][EQCLASS_NEWLINE]    = LS_EQUALS_TERMINAL,
+  [LS_SEEN_EQUALS][EQCLASS_IDENT]      = LS_EEUALS_TERMINAL,
+  [LS_SEEN_EQUALS][EQCLASS_NUMBER]     = LS_EQUALS_TERMINAL,
+  [LS_SEEN_EQUALS][EQCLASS_EQUALS]     = LS_OPERATOR,
+  [LS_SEEN_EQUALS][EQCLASS_COLON]      = LS_OPERATOR,
+  [LS_SEEN_EQUALS][EQCLASS_MINUS]      = LS_OPERATOR,
+  [LS_SEEN_EQUALS][EQCLASS_DOT]        = LS_OPERATOR,
+  [LS_SEEN_EQUALS][EQCLASS_GT]         = LS_SEEN_IMPURE,
+  [LS_SEEN_EQUALS][EQCLASS_OPERATOR]   = LS_OPERATOR,
+  [LS_SEEN_COLON][EQCLASS_WHITESPACE]  = LS_COLON_TERMINAL,
+  [LS_SEEN_COLON][EQCLASS_NEWLINE]     = LS_COLON_TERMINAL,
+  [LS_SEEN_COLON][EQCLASS_IDENT]       = LS_COLON_TERMINAL,
+  [LS_SEEN_COLON][EQCLASS_NUMBER]      = LS_COLON_TERMINAL,
+  [LS_SEEN_COLON][EQCLASS_EQUALS]      = LS_OPERATOR,
+  [LS_SEEN_COLON][EQCLASS_COLON]       = LS_OPERATOR,
+  [LS_SEEN_COLON][EQCLASS_MINUS]       = LS_OPERATOR,
+  [LS_SEEN_COLON][EQCLASS_DOT]         = LS_OPERATOR,
+  [LS_SEEN_COLON][EQCLASS_GT]          = LS_OPERATOR,
+  [LS_SEEN_COLON][EQCLASS_OPERATOR]    = LS_OPERATOR,
+  [LS_SEEN_MINUS][EQCLASS_WHITESPACE]  = LS_OPERATOR_TERMINAL,
+  [LS_SEEN_MINUS][EQCLASS_NEWLINE]     = LS_OPERATOR_TERMINAL,
+  [LS_SEEN_MINUS][EQCLASS_IDENT]       = LS_OPERATOR_TERMINAL,
+  [LS_SEEN_MINUS][EQCLASS_NUMBER]      = LS_OPERATOR_TERMINAL,
+  [LS_SEEN_MINUS][EQCLASS_EQUALS]      = LS_OPERATOR,
+  [LS_SEEN_MINUS][EQCLASS_COLON]       = LS_OPERATOR,
+  [LS_SEEN_MINUS][EQCLASS_MINUS]       = LS_OPERATOR,
+  [LS_SEEN_MINUS][EQCLASS_DOT]         = LS_OPERATOR,
+  [LS_SEEN_MINUS][EQCLASS_GT]          = LS_SEEN_RARROW,
+  [LS_SEEN_MINUS][EQCLASS_OPERATOR]    = LS_OPERATOR,
+  [LS_SEEN_DOT][EQCLASS_WHITESPACE]    = LS_OPERATOR_TERMINAL,
+  [LS_SEEN_DOT][EQCLASS_NEWLINE]       = LS_OPERATOR_TERMINAL,
+  [LS_SEEN_DOT][EQCLASS_IDENT]         = LS_OPERATOR_TERMINAL,
+  [LS_SEEN_DOT][EQCLASS_NUMBER]        = LS_OPERATOR_TERMINAL,
+  [LS_SEEN_DOT][EQCLASS_EQUALS]        = LS_OPERATOR,
+  [LS_SEEN_DOT][EQCLASS_COLON]         = LS_OPERATOR,
+  [LS_SEEN_DOT][EQCLASS_MINUS]         = LS_OPERATOR,
+  [LS_SEEN_DOT][EQCLASS_DOT]           = LS_SEEN_DOT_2,
+  [LS_SEEN_DOT][EQCLASS_GT]            = LS_OPERATOR,
+  [LS_SEEN_DOT][EQCLASS_OPERATOR]      = LS_OPERATOR,
+  [LS_SEEN_RARROW][EQCLASS_WHITESPACE] = LS_RARROW_TERMINAL,
+  [LS_SEEN_RARROW][EQCLASS_NEWLINE]    = LS_RARROW_TERMINAL,
+  [LS_SEEN_RARROW][EQCLASS_IDENT]      = LS_RARROW_TERMINAL,
+  [LS_SEEN_RARROW][EQCLASS_NUMBER]     = LS_RARROW_TERMINAL,
+  [LS_SEEN_RARROW][EQCLASS_EQUALS]     = LS_OPERATOR,
+  [LS_SEEN_RARROW][EQCLASS_COLON]      = LS_OPERATOR,
+  [LS_SEEN_RARROW][EQCLASS_MINUS]      = LS_OPERATOR,
+  [LS_SEEN_RARROW][EQCLASS_DOT]        = LS_OPERATOR,
+  [LS_SEEN_RARROW][EQCLASS_GT]         = LS_OPERATOR,
+  [LS_SEEN_RARROW][EQCLASS_OPERATOR]   = LS_OPERATOR,
+  [LS_SEEN_IMPURE][EQCLASS_WHITESPACE] = LS_IMPURE_TERMINAL,
+  [LS_SEEN_IMPURE][EQCLASS_NEWLINE]    = LS_IMPURE_TERMINAL,
+  [LS_SEEN_IMPURE][EQCLASS_IDENT]      = LS_IMPURE_TERMINAL,
+  [LS_SEEN_IMPURE][EQCLASS_NUMBER]     = LS_IMPURE_TERMINAL,
+  [LS_SEEN_IMPURE][EQCLASS_EQUALS]     = LS_OPERATOR,
+  [LS_SEEN_IMPURE][EQCLASS_COLON]      = LS_OPERATOR,
+  [LS_SEEN_IMPURE][EQCLASS_MINUS]      = LS_OPERATOR,
+  [LS_SEEN_IMPURE][EQCLASS_DOT]        = LS_OPERATOR,
+  [LS_SEEN_IMPURE][EQCLASS_GT]         = LS_OPERATOR,
+  [LS_SEEN_IMPURE][EQCLASS_OPERATOR]   = LS_OPERATOR,
+  [LS_SEEN_IMPURE][EQCLASS_TICK]       = LS_OPERATOR,
+  [LS_SEEN_DOT_2][EQCLASS_WHITESPACE]  = LS_OPERATOR_TERMINAL,
+  [LS_SEEN_DOT_2][EQCLASS_NEWLINE]     = LS_OPERATOR_TERMINAL,
+  [LS_SEEN_DOT_2][EQCLASS_IDENT]       = LS_OPERATOR_TERMINAL,
+  [LS_SEEN_DOT_2][EQCLASS_NUMBER]      = LS_OPERATOR_TERMINAL,
+  [LS_SEEN_DOT_2][EQCLASS_EQUALS]      = LS_OPERATOR,
+  [LS_SEEN_DOT_2][EQCLASS_COLON]       = LS_OPERATOR,
+  [LS_SEEN_DOT_2][EQCLASS_MINUS]       = LS_OPERATOR,
+  [LS_SEEN_DOT_2][EQCLASS_DOT]         = LS_SEEN_DOT_3,
+  [LS_SEEN_DOT_2][EQCLASS_GT]          = LS_OPERATOR,
+  [LS_SEEN_DOT_2][EQCLASS_OPERATOR]    = LS_OPERATOR,
+  [LS_SEEN_DOT_3][EQCLASS_WHITESPACE]  = LS_ELLIPSIS_TERMINAL,
+  [LS_SEEN_DOT_3][EQCLASS_NEWLINE]     = LS_ELLIPSIS_TERMINAL,
+  [LS_SEEN_DOT_3][EQCLASS_IDENT]       = LS_ELLIPSIS_TERMINAL,
+  [LS_SEEN_DOT_3][EQCLASS_NUMBER]      = LS_ELLIPSIS_TERMINAL,
+  [LS_SEEN_DOT_3][EQCLASS_EQUALS]      = LS_OPERATOR,
+  [LS_SEEN_DOT_3][EQCLASS_COLON]       = LS_OPERATOR,
+  [LS_SEEN_DOT_3][EQCLASS_MINUS]       = LS_OPERATOR,
+  [LS_SEEN_DOT_3][EQCLASS_DOT]         = LS_OPERATOR,
+  [LS_SEEN_DOT_3][EQCLASS_GT]          = LS_OPERATOR,
+  [LS_SEEN_DOT_3][EQCLASS_OPERATOR]    = LS_OPERATOR,
+  [LS_OPERATOR][EQCLASS_WHITESPACE]    = LS_OPERATOR_TERMINAL,
+  [LS_OPERATOR][EQCLASS_NEWLINE]       = LS_OPERATOR_TERMINAL,
+  [LS_OPERATOR][EQCLASS_IDENT]         = LS_OPERATOR_TERMINAL,
+  [LS_OPERATOR][EQCLASS_NUMBER]        = LS_OPERATOR_TERMINAL,
+  [LS_OPERATOR][EQCLASS_EQUALS]        = LS_OPERATOR,
+  [LS_OPERATOR][EQCLASS_COLON]         = LS_OPERATOR,
+  [LS_OPERATOR][EQCLASS_MINUS]         = LS_OPERATOR,
+  [LS_OPERATOR][EQCLASS_DOT]           = LS_OPERATOR,
+  [LS_OPERATOR][EQCLASS_GT]            = LS_OPERATOR,
+  [LS_OPERATOR][EQCLASS_OPERATOR]      = LS_OPERATOR,
   /* */
 };
 
@@ -189,39 +256,9 @@ lex_unit(struct unit *unit) {
       /* Determine the next state to go to. */
       eq_class = eqivalences[c];
       state = transitions[state][eq_class].state;
-      /* Force use of conditional moves on supported arches to speed things up.
-         With regards to Linus's rant on the topic of `cmov`: although in older
-         CPUs with more aggressive OoO he is correct in his assessment that
-         `cmov` may perform just as well if not worse than predicted branches,
-         modern architectures appear to no longer have that issue
-         (see https://github.com/xiadz/cmov), and since this is a lexer,
-         branching in this SM loop will be unpredictable so lots of recovery
-         would happen. */
-#if defined _ARCH_X86_
-      asm volatile (
-        "\tcmp %[state], %[terminals]\n"
-        "\tcmovl %[type], %[type]\n"
-        "\tcmovg %[new_type], %[type]\n"
-        : [type] "+r" (result->type)
-        : [state] "r" (state),
-          [terminals] "N" (LS_TERMINALS),
-          [new_type] "r" (new_type)
-      );
-#elif defined _ARCH_ARM_
-      asm volatile (
-        "\tcmp %[state], %[terminals]\n"
-        "\tmovlt %[type], %[type]\n"
-        "\tmovgt %[type], %[new_type]\n"
-        : [type] "+r" (result->type)
-        : [state] "r" (state),
-          [terminals] "N" (LS_TERMINALS),
-          [new_type] "r" (new_type)
-      );
-#else
-      result->type = transitions[state][eq_class].type;
-#endif
       result->len += stateful_increments[state];
     } while (state > LS_TERMINALS);
+    result->type = -state;
   }
 _exit:
   /* We're done, do nothing. */
