fabec0387e8304ebdc7f446147011de221f563f8
microchips-n-dip <xisavariable52@gmail.com>
2019-06-15 18:28:22 -0400

    Got the very speedy new lexer proposal working.

--- lex2.c
+++ lex2.c
@@ -6,94 +6,45 @@
    date: 2019-06-12
    */
 
-enum equivalence_classes {
-  EQCLASS_WHITESPACE,
-  EQCLASS_NEWLINE,
-  EQCLASS_LETTER,
-  EQCLASS_NUMBER,
-  EQCLASS_EQUALS,
-  EQCLASS_COLON,
-  EQCLASS_MINUS,
-  EQCLASS_DOT,
-  EQCLASS_GT,
-  EQCLASS_OPERATOR
-};
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
-static const char
-equivalence_class[256] = {
-  [' ']         = EQCLASS_WHITESPACE,
-  ['\t']        = EQCLASS_WHITESPACE,
-  ['\n']        = EQCLASS_NEWLINE,
-  ['A' ... 'Z'] = EQCLASS_LETTER,
-  ['a' ... 'z'] = EQCLASS_LETTER,
-  ['0' ... '9'] = EQCLASS_NUMBER,
-  ['=']         = EQCLASS_EQUALS,
-  [':']         = EQCLASS_COLON,
-  ['+']         = EQCLASS_OPERATOR,
-  ['-']         = EQCLASS_MINUS,
-  ['*']         = EQCLASS_OPERATOR,
-  ['/']         = EQCLASS_OPERATOR,
-  ['.']         = EQCLASS_DOT,
-  ['>']         = EQCLASS_GT,
-  ['<']         = EQCLASS_OPERATOR,
-};
+#include "alias.h"
+#include "lex.h"
 
-enum lexer_state {
-  /* Terminal states. */
-  LS_NEWLINE_TERMINAL  = -10,
-  LS_IDENT_TERMINAL    = -9,
-  LS_NUMBER_TERMINAL   = -8,
-  LS_EQUALS_TERMINAL   = -7,
-  LS_COLON_TERMINAL    = -6,
-  LS_RARROW_TERMINAL   = -5,
-  LS_IMPURE_TERMINAL   = -4,
-  LS_ELLIPSIS_TERMINAL = -3,
-  LS_OPERATOR_TERMINAL = -2,
-  LS_TERMINALS         = -1,
-  LS_START             =  0,
-  LS_WHITESPACE,   /* Whitespace, to be skipped. */
-  /* Normal textual lexing. */
-  LS_IDENT,
-  LS_NUMBER,
-  LS_STRING,
-  LS_CHAR,
-  /* Operator lexing states. */
-  /* Single char operators. */
-  LS_SEEN_EQUALS,   /* "=" */
-  LS_SEEN_COLON,    /* ":" */
-  LS_SEEN_MINUS,    /* "-" */
-  LS_SEEN_RARROW,   /* "->" */
-  LS_SEEN_IMPURE,   /* "=>" */
-  LS_SEEN_DOT,      /* "." */
-  LS_SEEN_DOT_2,    /* ".." */
-  /* Triple char operators. */
-  LS_SEEN_ELLIPSIS, /* "..." */
-  LS_OPERATOR,      /* Generic operator. */
-  LS_SEEN_TICK      /* "'" */
-};
-
-/* Total number of states. */
-
-static const char LS_LENGTH = LS_SEEN_TICK;
-
-/* State machine transitions table. */
-
-#include "lex_transitions.h"
-
-/* Stateful increments prevent incrementing on terminal states and
-   whitespace. */
-
-static const char
-stateful_increments[LS_LENGTH] = {
-  [LS_START ... LS_LENGTH] = 1
-};
+/* Initialize a tokenrun. */
 
 void
-lex_unit(struct unit *unit) {
+_init_tokenrun(tokenrun *run) {
+  run->next = NULL;
+  run->limit = run->tokens + TOKENRUN_SIZE;
+}
+
+/* Return the next tokenrun, and allocate it if necessary. */
+
+tokenrun *
+next_tokenrun(tokenrun *run) {
+  if (run->next == NULL) {
+    run->next = XNEW(tokenrun);
+    run->next->prev = run;
+    _init_tokenrun(run->next);
+  }
+  return run->next;
+}
+
+#include "lex_tables.h"
+
+/* Lex an entire unit. */
+
+void
+lex_unit(Unit *unit) {
   enum lexer_state state;
   token *result;
   char c;
-  int eq_class, disambiguation_hash;
+  int eq_class;
+  struct timespec tp0, tp1;
   while (1) {
     /* Prepare a new token from the tokenrun. */
     result = unit->cur_token++;
@@ -102,21 +53,49 @@ lex_unit(struct unit *unit) {
       unit->cur_token = unit->cur_run->tokens;
     }
     /* Check whether we've reached EOF. */
-    if (unlikely(i == unit->len)) {
+    if (unlikely(unit->cur == unit->rlimit)) {
       result->type = TOKEN_EOF;
       goto _exit;
     }
     state = LS_START;
+    result->len = 0;
     do {
-      c = *unit->cur++;
+      c = *unit->cur;
       /* Determine the next state to go to. */
-      eq_class = eqivalences[c];
-      state = transitions[state][eq_class].state;
-      result->len += stateful_increments[state];
-    } while (state > LS_TERMINALS);
-    result->type = -state;
+      eq_class = equivalence_class[c];
+      state = transitions[state][eq_class];
+      result->len += stateful_slice_increments[state];
+      /* In the case of increments, we need to reset whenever we've got a
+         newline otherwise we'll count empty lines. */
+#if defined _ARCH_X86_
+      asm volatile (
+        "\tcmpl\t%[eqclass], %[EQCLASS_NEWLINE]\n"
+        "\tcmove\t%[len], $0\n"
+        : [len] "=r" (result->len)
+        : [eqclass] "r" (eq_class),
+          [EQCLASS_NEWLINE] "N" (EQCLASS_NEWLINE)
+      );
+#else
+      result->len = (eq_class == EQCLASS_NEWLINE) ? 0 : result->len;
+#endif
+      unit->cur += stateful_char_increments[state];
+    } while (state < LS_NONTERMINALS_END);
+    result->type = state - LS_NONTERMINALS_END;
   }
 _exit:
   /* We're done, do nothing. */
   do {} while (0);
+}
+
+/* Destroy a file reader unit. */
+
+void
+destroy_unit(Unit *unit) {
+  /* Just remove the tokenruns for now. */
+  tokenrun *run;
+  while (unit->cur_run->prev) {
+    run = unit->cur_run;
+    unit->cur_run = run->prev;
+    XDELETE(run);
+  }
 }
