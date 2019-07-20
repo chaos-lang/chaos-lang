2a2a161672fe6e5c7502f4260c8959fb0692b148
microchips-n-dip <xisavariable52@gmail.com>
2019-07-20 20:45:24 +0000

    Applied fixes to xxHash after testing

--- lib/xxhash.c
+++ lib/xxhash.c
@@ -5,6 +5,11 @@
    date: 2019-07-19
    */
 
+#include <stdlib.h>
+#include <string.h>
+
+#include "xxhash.h"
+
 /* TODO: Comment, add compiler-specific requirements, optimize. */
 
 typedef unsigned char  BYTE;
@@ -20,7 +25,7 @@ little_endian(void) {
   } one = {1};
   return one.c[0];
 }
-#define CPU_LITTLE_ENDIAN little_endian();
+#define CPU_LITTLE_ENDIAN little_endian()
 #endif
 
 #define FORCE_INLINE static inline __attribute__((always_inline))
@@ -41,6 +46,13 @@ enum alignment {
   unaligned
 };
 
+static U32
+read32(const void *p) {
+  U32 val;
+  memcpy(&val, p, sizeof(val));
+  return val;
+}
+
 FORCE_INLINE U32
 read_LE32(const void *ptr) {
   return CPU_LITTLE_ENDIAN ? read32(ptr) : swap32(read32(ptr));
@@ -55,13 +67,6 @@ read_LE32_align(const void *ptr, enum alignment align) {
 }
 
 #define get32bits(p) read_LE32_align(p, align)
-
-static U32
-read32(const void *p) {
-  U32 val;
-  memcpy(&val, p, sizeof(val));
-  return val;
-}
 
 static const U32 PRIME32_1 = 2654435761U;
 static const U32 PRIME32_2 = 2246822519U;
@@ -146,13 +151,13 @@ endian_align_32(const void *input, size_t len, U32 seed,
       v3 = round32(v3, get32bits(p)); p += 4;
       v4 = round32(v4, get32bits(p)); p += 4;
     } while (p < limit);
-    h32 = rotl32(v1, 1) + rotl32(v2, 7) + rotl32(v4, 12) + rotl(v4, 18);
+    h32 = rotl32(v1, 1) + rotl32(v2, 7) + rotl32(v4, 12) + rotl32(v4, 18);
   }
   else {
     h32 = seed + PRIME32_5;
   }
   h32 += (U32) len;
-  return finalize32(h32, p, len & 15);
+  return finalize32(h32, p, len & 15, align);
 }
 
 unsigned int
---
+++ lib/xxhash.h
@@ -0,0 +1,3 @@
+#include <stddef.h>
+
+extern unsigned int xxhash32(const void *, size_t, unsigned int);
