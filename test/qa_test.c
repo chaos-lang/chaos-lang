d2a2ab4b422069c4a82d8bff24dd6231b52025e2
microchips-n-dip <xisavariable52@gmail.com>
2019-07-20 16:26:32 +0000

    Tested some more

--- test/qa_test.c
+++ test/qa_test.c
@@ -4,11 +4,24 @@
 
 int main(void) {
   quick_alloc_init();
-  int *a = quick_alloc(128);
-  int *b = quick_alloc(128);
-  int *c = quick_alloc(2056);
-  printf("a = %p\n", a);
-  printf("b = %p\n", b);
-  printf("c = %p\n", c);
+  int *a = quick_alloc(128 * sizeof(int));
+  int *b = quick_alloc(128 * sizeof(int));
+  int *c = quick_alloc(2056 * sizeof(int));
+  int i;
+  for (i = 0; i < 128; i++)
+    a[i] = i;
+  for (i = 0; i < 128; i++)
+    b[i] = i + 128;
+  for (i = 0; i < 2056; i++)
+    c[i] = i + 256;
+  for (i = 0; i < 128; i++)
+    printf("%d ", b[i]);
+  printf("\n");
+  for (i = 128; i < 256; i++)
+    printf("%d ", c[i]);
+  printf("\n\n");
+  for (i = 0; i < 256; i++)
+    printf("%d ", a[i]);
+  printf("\n");
   qa_free_all();
 }
