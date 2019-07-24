9c319033194c6518a38cbcf1afae2f0e7e286a2d
microchips-n-dip <xisavariable52@gmail.com>
2019-07-24 14:14:22 -0400

    Tested hash tables with data

--- test/ht_test.c
+++ test/ht_test.c
@@ -3,25 +3,38 @@
 #include "xxhash.h"
 #include "hash_table.h"
 
+enum rid_code {
+  RID_INT,
+  RID_DOUBLE,
+  RID_FLOAT,
+  RID_VOID
+};
+
+#define HT_RID(n) (*(enum rid_code *) (n)->data)
+
 hash_func calc_hash = xxhash32;
 
 int main(void) {
   struct table table;
   struct node *node;
   create_table(&table, 1);
-  lookup(&table, "int", 3, INSERT);
-  lookup(&table, "double", 6, INSERT);
-  lookup(&table, "void", 4, INSERT);
+  node = lookup(&table, "int", 3, INSERT);
+  HT_RID(node) = RID_INT;
+  node = lookup(&table, "double", 6, INSERT);
+  HT_RID(node) = RID_DOUBLE;
+  node = lookup(&table, "void", 4, INSERT);
+  HT_RID(node) = RID_VOID;
   int i;
   for (i = 0; i < 2; i++) {
     node = lookup(&table, "int", 3, NO_INSERT);
-    printf("%s\n", node->str);
+    printf("%s => %d\n", node->str, HT_RID(node));
     node = lookup(&table, "double", 6, NO_INSERT);
-    printf("%s\n", node->str);
+    printf("%s => %d\n", node->str, HT_RID(node));
     node = lookup(&table, "void", 4, NO_INSERT);
-    printf("%s\n", node->str);
+    printf("%s => %d\n", node->str, HT_RID(node));
   }
   node = lookup(&table, "float", 5, INSERT);
-  printf("%s\n", node->str);
+  HT_RID(node) = RID_FLOAT;
+  printf("%s => %d\n", node->str, HT_RID(node));
   destroy_table(&table);
 }
